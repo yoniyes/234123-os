6d0996fc0168d220396a0b19390d87c5a62f625e
yoniyes <yoniyes@campus.technion.ac.il>
2016-11-09 15:18:59 +0200

    pattach + entry.S
    pattach.c and pattach.h skeleton created. sys_call_table entries added
    in entry.S for HW1 functions.

---
+++ OS/kernel/pattach.c
@@ -0,0 +1,34 @@
+/**
+ *	pattach.c
+ *	OS 234120 HW1
+ *	Yoni Yeshanov
+ */
+
+// #include "pattach.h"
+
+/**
+ *
+ *	Actual implementation.
+ *
+ */
+int sys_attach_proc (pid_t PID) {
+	
+}
+
+/**
+ *
+ *	Actual implementation.
+ *
+ */
+int sys_get_child_processes(pid_t* result, unsigned int max_length) {
+	
+}
+
+/**
+ *
+ *	Actual implementation.
+ *
+ */
+int sys_get_child_process_count() {
+	
+}
